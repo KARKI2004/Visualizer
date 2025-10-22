fbc94f6c882bfc3e85066b78b3af3c4d64cc7ac2
Suyog Name <karkisuyognp@gmail.com>
2025-10-22 00:26:51 -0500

    added ouline draft in doc folder.

---
+++ CS340_Project/Doc/draft_outline.py
@@ -0,0 +1,128 @@
+# main.py
+from config import *
+from data_handler import *
+from analytics import *
+
+def main():
+    pass  # Entry point logic goes here
+
+if __name__ == "__main__":
+    main()
+
+
+# config.py
+DATA_PATH = "./Input/car_data.csv"
+COLUMNS = ["Year", "Selling_Price", "Present_Price", "Kms_Driven",
+           "Fuel_Type", "Seller_Type", "Transmission", "Owner"]
+
+
+# data_handler.py
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import logging
+
+class DataHandler:
+    def load_data(self):
+        pass
+
+    def basic_summary(self):
+        pass
+
+    def visualize_histogram(self):
+        pass
+
+    def visualize_line_plot(self):
+        pass
+
+class CarDataProcessor(DataHandler):
+    def visualize_box_plot(self):
+        pass
+
+    def visualize_scatter_plot(self):
+        pass
+
+    def visualize_violin_plot(self):
+        pass
+
+    def query_data(self):
+        pass
+
+    def export_csv(self):
+        pass
+
+    def export_pickle(self):
+        pass
+
+
+# analytics.py
+import numpy as np
+import pandas as pd
+import math
+
+class VectorAnalyzer:
+    def get_position_vector(self):
+        pass
+
+    def get_unit_vector(self):
+        pass
+
+    def calculate_dot_product(self):
+        pass
+
+    def calculate_angle(self):
+        pass
+
+    def check_orthogonality(self):
+        pass
+
+class ProbabilityAnalyzer(VectorAnalyzer):
+    def calculate_joint_counts(self):
+        pass
+
+    def calculate_joint_probabilities(self):
+        pass
+
+    def calculate_conditional_probabilities(self):
+        pass
+
+    def calculate_summary_stats(self):
+        pass
+
+    def export_results(self):
+        pass
+
+
+# utils.py (Optional)
+def log_operations():
+    pass
+
+def save_visualization():
+    pass
+
+def handle_exceptions():
+    pass
+
+def apply_lambda():
+    pass
+
+def evaluate_expression():
+    pass
+
+
+# visualization.py (Optional)
+def histogram_plot():
+    pass
+
+def line_plot():
+    pass
+
+def scatter_plot():
+    pass
+
+def box_plot():
+    pass
+
+def violin_plot():
+    pass
